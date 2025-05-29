8a344679c517648155753deb469ab37bada09bd7
JuanUrrutiaMX <greyes.urrutia@gmail.com>
2025-05-29 09:22:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,7 @@ def formulario():
 def gracias():
     return '¡Gracias por enviar tu información!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
